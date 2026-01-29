5a29f3f451b9a2d29dafc5660e7e9ad7b1e6ef3f
Michael Corn <127465591+mcorn02@users.noreply.github.com>
2026-01-28 16:48:52 -0800

    Add lab1 implementation files

--- lab1/my_robot.cpp
+++ lab1/my_robot.cpp
@@ -2,44 +2,4 @@
 #include "my_robot.h"
 using namespace Pololu3piPlus32U4;
 
-Motors motors;
-
-MyRobot::MyRobot() {
-  
-}
-
-void MyRobot::turnLeft(short duration, short speed) {
-
-}
-
-void MyRobot::turnRight(short duration, short speed) {
-
-}
-
-void MyRobot::Halt() {
-
-}
-
-void MyRobot::moveForward(short distance, short speed) {
-
-}
-
-void MyRobot::moveBackward(short distance, short speed) {
-
-}
-
-void MyRobot::moveForwardTurningLeft(short distance, short speed) {
-
-}
-
-void MyRobot::moveForwardTurningRight(short distance, short speed) {
-
-}
-
-void MyRobot::moveBackwardTurningLeft(short distance, short speed) {
-
-}
-
-void MyRobot::moveBackwardTurningRight(short distance, short speed) {
-
-}
+MyRobot::MyRobot() {}
