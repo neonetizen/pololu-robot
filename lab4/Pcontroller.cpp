4f16704ececf7e5eae9400f95b19d43854491c24
mcorn02 <michaelcorn02@gmail.com>
2026-02-18 11:22:11 -0800

    Pcontroller pre lab draft

--- lab4/Pcontroller.cpp
+++ lab4/Pcontroller.cpp
@@ -4,9 +4,23 @@ using namespace Pololu3piPlus32U4;
 
 Pcontroller::Pcontroller(float kp, double minOutput, double maxOutput) {
   // initialize the private varaibles from Pcontroller.h here
+  _kp = kp;
+  _minOutput = minOutput;
+  _maxOutput = maxOutput;
+  _error = 0;
+  _target_value = 0;
+  _value = 0;
 }
 
 double Pcontroller::update(double value, double target_value){
   //Controller math here
   //Hint: Need to return actuator controller value (_clampOut)
+  _error = target_value - value;
+  _clampOut = _kp * _error;
+  if (_clampOut > _maxOutput) {
+    _clampOut = _maxOutput;
+  } else if (_clampOut < _minOutput) {
+    _clampOut = _minOutput;
+  }
+  return _clampOut;
 }
--- lab4/Pcontroller.h
+++ lab4/Pcontroller.h
@@ -6,12 +6,16 @@ using namespace Pololu3piPlus32U4;
 class Pcontroller{
   public:
     Pcontroller(float kp, double minOutput, double maxOutput);
-    double update(double value, double target_value); 
-    
+    double update(double value, double target_value);     
   private:
     //Add private variables here
     //Hint: There should be 6 variables in total
-	
+    float _kp; // proportional gain
+    double _minOutput; // lower clamp bound
+    double _maxOutput; // upper clamp bound
+    double _target_value; // target value
+    double _value; // sensor value
+    double _clampOut; // clamped output value
 };
 
 #endif
