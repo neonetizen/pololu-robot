1a47ce7e25fc808ff627e08a0801f3ae948f778d
mcorn02 <michaelcorn02@gmail.com>
2026-03-12 16:48:48 -0700

    Working section 2.1

--- lab7/lab7.ino
+++ lab7/lab7.ino
@@ -1,11 +1,12 @@
 #include <Pololu3piPlus32U4.h>
 #include "printOLED.h"
-//#include "odometry.h" //If using odometry, import odometry.h and odometry.cpp
-//#include "PIDcontroller.h" //Import your PIDcontroller.h and PIDcontroller.cpp from last lab then uncomment
+#include "odometry.h" //If using odometry, import odometry.h and odometry.cpp
+#include "PIDcontroller.h" //Import your PIDcontroller.h and PIDcontroller.cpp from last lab then uncomment
 using namespace Pololu3piPlus32U4;
 
 Motors motors;
 Encoders encoders;
+PrintOLED oledPrinter;
 
 //Odometry Parameters
 #define diaL 3.2
@@ -17,32 +18,32 @@ Encoders encoders;
 #define DEAD_RECKONING false
 
 //Update kp, kd, and ki based on your testing (First PIDcontroller for angle)
-#define minOutputAng -100
-#define maxOutputAng 100
-#define kpAng ... //Tune Kp here
-#define kdAng ... //Tune Kd here
-#define kiAng ... //Tune Ki here
-#define clamp_iAng ... //Tune ki integral clamp here
-#define base_speedAng 50
+#define minOutputAng -50.0
+#define maxOutputAng 50.0
+#define kpAng 10.0f //Tune Kp here
+#define kdAng 5.0f //Tune Kd here
+#define kiAng 0.75f //Tune Ki here
+#define clamp_iAng 10.0f //Tune ki integral clamp here
+#define base_speedAng 150
 
 //Update kp, kd, and ki based on your testing (Second PIDcontroller for velocity) (Task 2.3)
 #define minOutputVel -100
 #define maxOutputVel 100
-#define kpVel ... //Tune Kp here
-#define kdVel ... //Tune Kd here
-#define kiVel ... //Tune Ki here
-#define clamp_iVel ... //Tune ki integral clamp here
+#define kpVel 10 //Tune Kp here
+#define kdVel 5 //Tune Kd here
+#define kiVel 0.75 //Tune Ki here
+#define clamp_iVel 10.0f //Tune ki integral clamp here
 #define base_speedVel 50
 
-//Odometry odometry(diaL, diaR, w, nL, nR, gearRatio, DEAD_RECKONING); //Uncomment if using odometry class
-//PIDcontroller pidcontroller(kpAng, kiAng, kdAng, minOutputAng, maxOutputAng, clamp_iAng); //Uncomment after you import PIDController
+Odometry odometry(diaL, diaR, w, nL, nR, gearRatio, DEAD_RECKONING); //Uncomment if using odometry class
+PIDcontroller pidcontroller(kpAng, kiAng, kdAng, minOutputAng, maxOutputAng, clamp_iAng); //Uncomment after you import PIDController
 //Write your second PIDcontroller object here (Task 2.3)
 
 //Feel free to use this in your PD/PID controller for target values
 // Given goals in cm and radians
-const float goal_x = ... 
-const float goal_y = ...
-const float goal_theta = ... // Must put in radians
+const float goal_x = 100;
+const float goal_y = 100;
+const float goal_theta = PI; // Must put in radians
 
 //odometry
 int16_t deltaL=0, deltaR=0;
@@ -54,6 +55,7 @@ float x, y, theta;
 double PIDout_theta, PIDout_distance; //Output variables for your controllers
 double angle_to_goal, actual_angle; //Keeping track of angle
 double dist_to_goal = 0.0; //Keeping track of robot's distance to goal location
+double PIDout;
 
 void setup() {
   Serial.begin(9600);
@@ -73,24 +75,31 @@ void loop() {
 
   odometry.update_odom(encCountsLeft,encCountsRight, x, y, theta); //calculate robot's position
 
-
   //Lab 7
   //Note: To help with testing, print the theta and PID outputs to serial monitor.
 
-  /*TASK 2.1
-  Move your PIDController.h and PIDController.cpp files here to use for the following tasks.
-  Also move your odometry.h and odometry.cpp if you decide to use it for 
-  measuring the angle of your robot.
+  //TASK 2.1
+  //Move your PIDController.h and PIDController.cpp files here to use for the following tasks.
+  //Also move your odometry.h and odometry.cpp if you decide to use it for 
+  //measuring the angle of your robot.
   
-  Utilize your PIDController to go to a specific location.
+  //Utilize your PIDController to go to a specific location.
   
-  Hint: Utilize these functions to find your thetas
-  angle_to_goal = atan2(?, ?);
+  //Hint: Utilize these functions to find your thetas
+  //angle_to_goal = atan2(?, ?);
   //atan2(sin(x),cos(x))=x on [-π, π) and not on [0,2π) 
   //=> we do this to make sure the range of actual_angle and goal_to_angle is the same
-  actual_angle = atan2(?, ?);
+  //actual_angle = atan2(?, ?);
+
+  angle_to_goal = atan2(goal_y - y, goal_x - x);
+  actual_angle = atan2(cos(theta), sin(theta));
   
-  Write your code below and comment out when moving to the next task.*/
+  PIDout = pidcontroller.update(angle_to_goal, actual_angle);
+
+  int16_t left  = constrain((int16_t)calculateLeft(PIDout), -400, 400);
+  int16_t right = constrain((int16_t)calculateRight(PIDout), -400, 400);
+  motors.setSpeeds(left, right);  
+  
 
   /*TASK 2.2
   Improve the baseline solution by telling the robot to stop when it gets close 
@@ -102,5 +111,12 @@ void loop() {
   as it goes towards the goal.
   Write your code below.*/
 
+}
 
+static inline double calculateRight(double angV) {
+  return base_speedVel + (w * angV / 2);
+}
+
+static inline double calculateLeft(double angV) {
+  return base_speedVel - (w * angV / 2);
 }
